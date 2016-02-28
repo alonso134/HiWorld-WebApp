768834ecb5a79e081e850dac20cbfe9798252fd1
Kaichen Xiao <xiaokaic@msu.edu>
2016-02-28 07:32:47 -0500

    initial commit

---
+++ AjaxPOSTRequest.js
@@ -0,0 +1,26 @@
+function postData(input) {
+    document.getElementById("info").innerHTML = "Analyzing Information, Please Stand By...";
+    // send POST request using Ajax
+    $.ajax({
+        type: "POST",
+        url: "http://discoverworld.me/cgi-bin/test.py",
+        data: input,
+        success: callbackFunc
+    })
+}
+
+function callbackFunc(response) {
+    // do something with the response
+    var info = response;
+    console.log(response);
+    console.log(typeof(response));
+    document.getElementById("info").innerHTML = response;
+}
+
+// call function when click the button
+$(document).ready(function(){
+    $('#go').click(function(){
+        postData($('#urlForm').val());
+        $("#myModal").modal();
+    });
+});
